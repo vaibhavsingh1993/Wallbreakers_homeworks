191301cb92a800ba36a1d152bec74a8507cfaf86
Vaibhav Singh <vaibhavsinghacads@gmail.com>
2020-05-24 10:44:24 -0400

    Update TwoSum.java

--- hw1/TwoSum.java
+++ hw1/TwoSum.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> cache = new HashMap();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (cache.get(target - nums[i]) != null) {
-                return new int[] { cache.get(target - nums[i]), i };
+        Map<Integer,Integer> map = new HashMap();
+        for (int i = 0; i < nums.length;i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
-            cache.put(nums[i], i);
+            map.put(nums[i], i);
         }
-        return new int[2];
+        return new int[]{};
     }
 }
